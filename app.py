1e9528ee651b8b271f1ac8036193901d3ff984d3
GEMLabHKU <gem61825@gmail.com>
2025-08-21 18:01:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import shutil
 import os
 import streamlit as st
 import re
-from langchain_openai import ChatOpenAI
+# from langchain_openai import ChatOpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import OpenAIEmbeddings
